0c09bf4e00d291d8bbcee09b42a9f50c2f5f026c
Nathan Brownstein <nana8901@gmail.com>
2022-09-08 14:50:34 -0400

    added show, added route from index to show

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,22 @@ class CatsController < ApplicationController
     @cats = Cat.all
     render 'cats/index'
   end
+  def show
+    @cat = Cat.find_by(id: params[:id])
+    render 'cats/show'
+  end
+  def new    
+    render 'cats/new'
+  end
+
+  def create
+    cat = Cat.new(
+      name: params[:name],
+      breed: params[:breed],
+      image_url: params[:image_url],
+      age: params[:age]
+    )
+    cat.save
+    redirect_to "/cats"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/cats" => "cats#index"
+  get "/cats/:id" => "cats#show"
 end
